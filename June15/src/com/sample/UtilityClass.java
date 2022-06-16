4da29375ff7f66952f6cdc2dd842c1f331de01a6
unknown <santosh123@gmail.com>
2022-06-16 20:09:57 +0530

    java

---
+++ June15/src/com/sample/UtilityClass.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class UtilityClass {
+	
+	
+	private void school() {
+
+		System.out.println("sCHOOL Name: -Bharath University");
+	}
+	public static void main(String[] args) {
+		
+	
+	UtilityClass  c = new UtilityClass();
+	
+	c.school();
+	}
+	
+}
